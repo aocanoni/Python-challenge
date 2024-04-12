15705ed7b9d2b9d777e76905cd293e4cb635a887
aocanoni <157078630+aocanoni@users.noreply.github.com>
2024-04-11 18:13:11 -0700

    Added Starter_Code
    added starter_code file to empty files (pybank and pypoll)

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,23 @@
+#os module to let create paths across operating systems?
+import os
+
+#module for reading CSV files
+import csv
+
+csvpath = os.path.join('PyBank', 'Resources', 'budget_data.csv')
+
+#improved reading using CSV module?
+
+with open(csvpath) as csvfile:
+
+    #csv reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter= ',')
+
+    #read the header row first
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    #read each row of dta after the header
+    for row in csvreader:
+        print(row)
 
